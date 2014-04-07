0a85be61ad1c7af0ca838cd696b2510a91666468
wentianle <wenzaiel@163.com>
2014-04-08 00:25:06 +0800

    fix_verify_ac error

--- lib/ucloud/ruby/sdk/sdk.rb
+++ lib/ucloud/ruby/sdk/sdk.rb
@@ -1,48 +1,50 @@
 require 'digest/sha1'
 
 class UcloudApiClient
-  class << self
-    attr_accessor :public_key, :private_key, :base_url
-    def configure
-      yield self
+    class << self
+        attr_accessor :public_key, :private_key, :base_url
+        def configure
+            yield self
+        end
     end
-  end
-  def initialize()
-    #@base = "https://api.ucloud.cn"
-    #@private_key = "a898f7620bb12f8af80dd32c17cdec2e2111993b"
-  end
-  def get(url, params, format="json")
-    params = params.clone
-    params['public_key'] = UcloudApiClient.public_key
-    params["region_id"] = 1
-    params["zone_id"] = 1
-    params["project_id"] = 1
-    params["module_id"] = 1
+    def initialize()
+        #@base = "https://api.ucloud.cn"
+        #@private_key = "a898f7620bb12f8af80dd32c17cdec2e2111993b"
+    end
+    def get(url, params, format="json")
+        params = params.clone
+        params['public_key'] = UcloudApiClient.public_key
+        params["region_id"] = 1
+        params["zone_id"] = 1
+        params["project_id"] = 0
+        params["module_id"] = 0
+        params["format"] = format
 
-    params["access_token"] = verify_ac(params)
-    result = RestClient.get "#{UcloudApiClient.base_url}#{url}", {:params => params.merge("format" => format)}
-    puts result
-    result
-  end
+        params["access_token"] = verify_ac(params)
 
-  def post
 
-  end
+        result = RestClient.get "#{UcloudApiClient.base_url}#{url}", {:params => params}
+        result
+    end
 
-  private
+    def post
+
+    end
+
+    private
     def verify_ac(params)
-      params = params.clone
-
-      params_data = ""
-      params.sort.each do |key, value|
-        params_data = params_data + key.to_s + value.to_s
-
-        params_data = params_data + UcloudApiClient.private_key
-
         #'''use sha1 to encode keys'''
+        params = params.clone
 
-      end
-      Digest::SHA1.hexdigest params_data
+        params_data = ""
+        params.sort.each do |key, value|
+            params_data += (key.to_s + value.to_s)
+        end
+
+        params_data += UcloudApiClient.private_key
+
+        #puts params_data
+        Digest::SHA1.hexdigest params_data
     end
 
 end
